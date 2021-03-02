ffceac002f8179de0d07632c85228fcae785ce1b
chinayx <chinayx@gmail.com>
2021-03-02 08:53:27 +0800

    modify sed param

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.123.254/g' package/base-files/files/bin/config_generate
 #Modify target to kernel 5.4
-sed -i 's/4.19/5.4/G' target/linux/armvirt/Makefile
+sed -i 's/4.19/5.4/g' target/linux/armvirt/Makefile
