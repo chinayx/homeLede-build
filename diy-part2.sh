9150ac72f70b7ea92a7e7175cd56f67ef6c7f0c8
chinayx <chinayx@gmail.com>
2021-03-03 10:36:13 +0800

    add homeclash

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,8 @@
 #============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.123.254/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.254/g' package/base-files/files/bin/config_generate
 #Modify target to kernel 5.4
 sed -i 's/4.19/5.4/g' target/linux/armvirt/Makefile
+sed -i 's/=x86_64/=all/g' package/feeds/homeclash/luci-app-openclash/Makefile
+sed -i 's/\@TARGET_x86_64//g' package/feeds/homeclash/luci-app-openclash/Makefile
